a4f71814fae463bafff61c58be379198b3d3ce0b
Student <example@example.com>
2020-01-30 14:46:31 -0800

    semi functioning fronte end

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 
 Rails.application.routes.draw do
   root 'destinations#index'
-  resources :destinations do
-    resources :reviews
+  resources :destinations
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-end
